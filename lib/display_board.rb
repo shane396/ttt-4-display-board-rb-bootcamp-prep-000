ee6094ab7648819337e9d019ad4e23f662ccd8ec
Shane Allison <shane@publicaddress.com.au>
2019-07-03 05:25:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
-end
-
-puts board
-
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+display_board
