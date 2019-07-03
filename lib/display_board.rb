c842507d0b23ac03cb0fedaa4e9ca0c0d4a76307
Shane Allison <shane@publicaddress.com.au>
2019-07-03 05:33:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,11 +4,11 @@
 board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board
-  #{board[0]},| #{board[1]},|#{board[2]}
-  "-----------"
-   #{board[3]},| #{board[4]},|#{board[5]}
-    "-----------"
-   #{board[6]},| #{board[7]},|#{board[8]} 
+ puts #{board[0]},| #{board[1]},|#{board[2]}
+ puts "-----------"
+ puts #{board[3]},| #{board[4]},|#{board[5]}
+ puts   "-----------"
+ puts #{board[6]},| #{board[7]},|#{board[8]} 
 end
   
 display_board(board)
