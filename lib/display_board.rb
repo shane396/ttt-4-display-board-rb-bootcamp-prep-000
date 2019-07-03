ea6baabdc58d1b3686d60f7643acebb0963209cc
Shane Allison <shane@publicaddress.com.au>
2019-07-03 05:31:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,10 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board
   #{board[0]},| #{board[1]},|#{board[2]}
-  
+  "-----------"
+   #{board[3]},| #{board[4]},|#{board[5]}
+    "-----------"
+   #{board[6]},| #{board[7]},|#{board[8]} 
 end
-  
   
 display_board(board)
